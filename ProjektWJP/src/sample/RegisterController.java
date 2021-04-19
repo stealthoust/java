4b63222fba25562c5f00bd1bc229f005f7bc62de
stealthoust <thoustx9@gmail.com>
2021-04-19 09:32:09 +0200

    Projekt WJP
    Login and Register forms are almost done

--- ProjektWJP/src/sample/RegisterController.java
+++ ProjektWJP/src/sample/RegisterController.java
@@ -1,17 +1,21 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
@@ -26,6 +30,14 @@ private Label alertregister;
 private PasswordField ustawhaslo;
 @FXML
 private PasswordField ustawhaslo2;
+@FXML
+private Label alertpotwierdzhaslo;
+@FXML
+TextField poleimie;
+@FXML
+TextField polenazwisko;
+@FXML
+TextField ustawlogin;
 
     public RegisterController() {
     }
@@ -41,7 +53,18 @@ private PasswordField ustawhaslo2;
 
     public void registerButtonAction(ActionEvent event)
     {
-        alertregister.setText("Rejestracja przebiegła pomyślnie. Możesz zalogować się na swoje konto!");
+        if(ustawhaslo.getText().equals(ustawhaslo2.getText()))
+        {   rejestracjaUser();
+            alertpotwierdzhaslo.setText("");
+
+
+        }
+        else
+        {
+            alertpotwierdzhaslo.setText("Hasla sie nie zgadzaja!!");
+        }
+        //alertregister.setText("Rejestracja przebiegła pomyślnie. Możesz zalogować się na swoje konto!");
+
 
     }
 
@@ -49,20 +72,37 @@ private PasswordField ustawhaslo2;
     {
     Stage stage =(Stage) zamknijButton.getScene().getWindow();
     stage.close();
+    Platform.exit();
 
     }
 
     public void rejestracjaUser()
     {
-        if(ustawhaslo.getText().equals(ustawhaslo2.getText()))
+        sample.BazaDanychConnect polaczenie= new sample.BazaDanychConnect();
+        Connection polaczDB= polaczenie.getConnection();
+
+        String imie=poleimie.getText();
+        String nazwisko=polenazwisko.getText();
+        String login=ustawlogin.getText();
+        String haslo=ustawhaslo.getText();
+
+        String kodsql="INSERT INTO users (imie,nazwisko,login,haslo) VALUES('";
+        String wpisane=imie+"','"+nazwisko+"','"+login+"','"+haslo+"')";
+        String wpisanerejestracja= kodsql+wpisane;
+
+        try
         {
-
-
+            Statement statement = polaczDB.createStatement();
+            statement.executeUpdate(wpisanerejestracja);
+            alertregister.setText("Rejestracja przebiegła pomyślnie. Możesz zalogować się na swoje konto!");
         }
-        else
+        catch(Exception e)
         {
-
+            e.printStackTrace();
+            e.getCause();
         }
+
+
 
     }
 }
